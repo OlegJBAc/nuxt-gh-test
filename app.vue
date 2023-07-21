944b537f285a498a00f71985ef4b74cf025e3488
OlegJBAc <okarakulinjb@mail.ru>
2023-07-21 11:04:37 +0300

    initializing app

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <nav>
+        <ul>
+          <NuxtLink to="/">
+            <li>
+              <span>index page</span>
+            </li>
+          </NuxtLink >
+          <NuxtLink to="about">
+            <li>
+              <span>about page</span>
+            </li>
+          </NuxtLink>
+
+        </ul>
+      </nav>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
