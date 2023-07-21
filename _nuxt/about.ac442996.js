f1a5f855005d8c0fae31d5632dbf90cf6da1a387
OlegJBAc <okarakulinjb@mail.ru>
2023-07-21 11:05:51 +0300

    Updates

---
+++ _nuxt/about.ac442996.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./entry.22a78aed.js";const a={name:"about"};function c(n,r,s,p,_,u){return t(),o("h1",null,"About page")}const l=e(a,[["render",c]]);export{l as default};
---
+++ _nuxt/index.bb567471.js
@@ -0,0 +1,1 @@
+import{_ as e,c as n,o}from"./entry.22a78aed.js";const t={name:"index"};function c(a,r,s,p,_,d){return o(),n("h1",null,"Index page")}const i=e(t,[["render",c]]);export{i as default};
